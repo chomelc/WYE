5b58b57dac7540598e4c2c8b798c81eb9d660b56
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-01-20 11:05:54 +0100

    Fixing profile panel

--- frontend/src/components/ProfilePanel.tsx
+++ frontend/src/components/ProfilePanel.tsx
@@ -6,11 +6,15 @@ export default function ProfilePanel() {
     const [error, setError] = React.useState(null);
     const [isLoaded, setIsLoaded] = React.useState(false);
     const [users, setUsers] = React.useState<any[]>([]);
-    const [firstName, setFirstName] = React.useState("");
-    const [lastName, setLastName] = React.useState("");
-    const [initials, setInitials] = React.useState("");
+    let firstName;
+    let lastName;
+    let initials;
 
     useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    const fetchUsers = () => {
         fetch("http://192.168.0.10:5000/wye/users/")
             .then(res => res.json())
             .then(
@@ -19,51 +23,59 @@ export default function ProfilePanel() {
                     setUsers(data);
                 },
                 (error) => {
-                    setIsLoaded(true);
+                    setIsLoaded(false);
                     setError(error);
                 }
             )
-    }, [])
+    }
 
-    const username = ReactSession.get("username");
-    const json = JSON.stringify(users);
-    const json_users = JSON.parse(json);
+    if (error) {
+        return <div>Error: {error}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
 
-    var connected_user = json_users.find(o=>o.slug==username);
-    console.log(connected_user);
-    // setFirstName(connected_user.first_name);
-    // setLastName(connected_user.last_name);
-    // setInitials(connected_user.initials);
+        const username = ReactSession.get("username");
+        const json = JSON.stringify(users);
+        const json_users = JSON.parse(json);
 
-    return (
-        <Box
-            component="form"
-            sx={{
-                '& .MuiTextField-root': { m: 2, width: '100%' },
-                p: 2
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Grid container spacing={2} >
-                        <Grid item xs={4}>
-                            <Avatar>{initials}</Avatar>
-                        </Grid>
-                        <Grid item xs={8}>
-                            <Grid container direction="row-reverse">
-                                <Grid item>
-                                    <Typography variant="h5" component="div" gutterBottom>
-                                        {firstName} {lastName}
-                                    </Typography>
+        var connected_user = json_users.find(o => { return o.slug === username });
+        if (connected_user != undefined) {
+            firstName = connected_user.first_name;
+            lastName = connected_user.last_name;
+            initials = connected_user.initials;
+        }
+
+        return (
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 2, width: '100%' },
+                    p: 2
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Grid container spacing={2} >
+                            <Grid item xs={4}>
+                                <Avatar>{initials}</Avatar>
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Grid container direction="row-reverse">
+                                    <Grid item>
+                                        <Typography variant="h5" component="div" gutterBottom>
+                                            {firstName} {lastName}
+                                        </Typography>
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
-                    <Divider sx={{ marginTop: 2 }} variant="inset" />
-                </CardContent>
-            </Card>
-        </Box >
-    );
+                        <Divider sx={{ marginTop: 2 }} variant="inset" />
+                    </CardContent>
+                </Card>
+            </Box >
+        );
+    }
 }
