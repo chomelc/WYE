50edf8ee3f5fb10539d76c3383f54374cb7a4cee
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-01-23 22:06:01 +0100

    Implementing POST and PUT actions on grocery list

--- frontend/src/components/GroceryListPanel.tsx
+++ frontend/src/components/GroceryListPanel.tsx
@@ -4,7 +4,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import React, { useEffect } from "react";
 import { ReactSession } from 'react-client-session';
 import { useDispatch, useSelector } from "react-redux";
-import { deleteItem } from "../state/actions/items.actions";
+import { addItem, deleteItem, editItem } from "../state/actions/items.actions";
 
 const styles = {
     barredItem: {
@@ -15,9 +15,30 @@ const styles = {
 };
 
 export default function GroceryListPanel() {
+    const connected_user = ReactSession.get("username");
     const dispatch = useDispatch();
+    const [itemToAdd, setItemToAdd] = React.useState('');
     const items: IItem[] = useSelector(
         (state: ItemState) => state.items)
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setItemToAdd(event.target.value);
+    };
+
+    const handleDelete = (username: string, item: string) => evt => {
+        console.log("Deleting " + item + " from " + username + "'s list.");
+        dispatch(deleteItem(username, item));
+    }
+
+    const handleCheck = (username: string, item: string, isChecked: boolean) => evt => {
+        dispatch(editItem(username, item, isChecked));
+    }
+
+    const handleAdd = (username: string, item: string) => evt => {
+        console.log("Adding " + item + " to " + username + "'s list.");
+        dispatch(addItem(username, item));
+        setItemToAdd('');
+    }
 
     return (
         <Box
@@ -31,11 +52,11 @@ export default function GroceryListPanel() {
         >
             <Grid container spacing={2} alignContent="center" alignItems="center">
                 <Grid item xs={9}>
-                    <TextField id="add-item" label="Ajouter un item" variant="filled" color="success" />
+                    <TextField id="add-item" value={itemToAdd} label="Ajouter un item" variant="filled" color="success" onChange={handleChange} />
                 </Grid>
                 <Grid item xs={3}>
                     <Grid container direction="row-reverse">
-                        <IconButton color="success" aria-label="add an item">
+                        <IconButton color="success" onClick={handleAdd(connected_user, itemToAdd)} aria-label="add an item">
                             <AddCircleIcon fontSize="large" />
                         </IconButton>
                     </Grid>
@@ -47,14 +68,13 @@ export default function GroceryListPanel() {
                         <ListItem
                             key={item.slug}
                             secondaryAction={
-                                // onClick={dispatch(deleteItem(item.g_list.author.slug, item.slug))}
-                                <IconButton edge="end" aria-label="delete">
+                                <IconButton edge="end" aria-label="delete" onClick={handleDelete(item.g_list.author.slug, item.slug)}>
                                     <DeleteIcon />
                                 </IconButton>
                             }
                             disablePadding
                         >
-                            <ListItemButton role={undefined} dense>
+                            <ListItemButton role={undefined} onClick={handleCheck(item.g_list.author.slug, item.slug, !item.is_checked)} dense>
                                 <ListItemIcon>
                                     <Checkbox
                                         edge="start"
--- frontend/src/state/actions/items.actions.ts
+++ frontend/src/state/actions/items.actions.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 export const GET_ITEMS = "GET_ITEMS";
-export const DELETE_ITEM = "DELETE_ITEM";
 
 export const getItems = (username: string) => {
     return (dispatch) => {
-        return axios.get("http://192.168.0.10:5000/wye/groceries/"+username)
+        return axios.get("http://192.168.0.10:5000/wye/groceries/" + username)
             .then((res) => {
                 dispatch({ type: GET_ITEMS, payload: res.data })
             })
@@ -13,11 +12,31 @@ export const getItems = (username: string) => {
     }
 }
 
+export const addItem = (username: string, item: string) => {
+    return (dispatch) => {
+        return axios.post("http://192.168.0.10:5000/wye/groceries?list=" + username + "&item=" + item + "&is_checked=False")
+            .then((res) => {
+            dispatch(getItems(username))
+        })
+    .catch((err) => console.log(err))
+    }
+}
+
+export const editItem = (username: string, item: string, isChecked: boolean) => {
+    return (dispatch) => {
+        return axios.put("http://192.168.0.10:5000/wye/groceries/" + username + "/" + item + "?is_checked=" + isChecked)
+            .then((res) => {
+            dispatch(getItems(username))
+        })
+    .catch((err) => console.log(err))
+    }
+}
+
 export const deleteItem = (username: string, item: string) => {
     return (dispatch) => {
-        return axios.delete("http://192.168.0.10:5000/wye/groceries/"+username+"/"+item)
+        return axios.delete("http://192.168.0.10:5000/wye/groceries/" + username + "/" + item)
             .then((res) => {
-                dispatch({ type: GET_ITEMS, payload: res.data })
+                dispatch(getItems(username))
             })
             .catch((err) => console.log(err))
     }
