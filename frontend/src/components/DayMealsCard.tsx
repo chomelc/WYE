d185046fceb903bfd73900b256b2c68c716c5747
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-01-25 22:54:16 +0100

    Fixing DayMealsCard TextFields

--- frontend/src/components/DayMealsCard.tsx
+++ frontend/src/components/DayMealsCard.tsx
@@ -1,83 +1,107 @@
-import { Card, CardContent, Grid, Typography, Chip, Divider, List, ListItem, ListItemIcon, ListItemText, TextField } from "@mui/material";
+import { Card, CardContent, Grid, Typography, Chip, Divider, List, ListItem, ListItemIcon, ListItemText, TextField, Button } from "@mui/material";
 import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import CoffeeIcon from '@mui/icons-material/Coffee';
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { editBreakfast, editDinner, editLunch } from "../state/actions/day.actions";
+import { useNavigate } from "react-router-dom";
 
 export default function DayMealsCard(props) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const d: IDay = props.d;
-    const [breakfast, setBreakfast] = React.useState('');
 
-    const handleBFChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [breakfast, setBreakfast] = React.useState(d.breakfast.name);
+    const [lunch, setLunch] = React.useState(d.lunch.name);
+    const [dinner, setDinner] = React.useState(d.dinner.name);
+
+    const handleBFChange = (day: IDay) => (event: React.ChangeEvent<HTMLInputElement>) => {
         setBreakfast(event.target.value);
     };
 
-    const [lunch, setLunch] = React.useState('');
-
-    const handleLChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleLChange = (day: IDay) => (event: React.ChangeEvent<HTMLInputElement>) => {
         setLunch(event.target.value);
     };
 
-    const [dinner, setDinner] = React.useState('');
-
-    const handleDChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleDChange = (day: IDay) => (event: React.ChangeEvent<HTMLInputElement>) => {
         setDinner(event.target.value);
     };
 
+    const handleFinish = (day: IDay) => evt => {
+        console.log("Changin breakfast from '" + day.breakfast.name + "' to '" + breakfast + "'.");
+        // if there is already a breakfast, edit it
+        // if (day.breakfast.slug)
+        //     dispatch(editBreakfast(day.slug, breakfast));
+        console.log("Changin lunch from '" + day.lunch.name + "' to '" + lunch + "'.");
+        // if there is already a lunch, edit it
+        // if (day.lunch.slug)
+        //     dispatch(editLunch(day.slug, lunch));
+        console.log("Changin dinner from '" + day.dinner.name + "' to '" + dinner + "'.");
+        // if there is already a dinner, edit it
+        // if (day.dinner.slug)
+        //     dispatch(editDinner(day.slug, dinner));
+        evt.preventDefault();
+        navigate("/meals");
+    }
+
     return (
-        <Card sx={{ minWidth: 275, marginBottom: 2 }}>
-            <CardContent>
-                <Grid container spacing={2} >
-                    <Grid item xs={8}>
-                        <Typography variant="h5" component="div" gutterBottom>
-                            {d.day}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Grid container direction="row-reverse">
-                            <Grid item>
-                                <Chip label={d.date} variant="outlined" />
+        <div>
+            <Card sx={{ minWidth: 275, marginBottom: 2 }}>
+                <CardContent>
+                    <Grid container spacing={2} >
+                        <Grid item xs={8}>
+                            <Typography variant="h5" component="div" gutterBottom>
+                                {d.day}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Grid container direction="row-reverse">
+                                <Grid item>
+                                    <Chip label={d.date} variant="outlined" />
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
-                <Divider />
-                <List>
-                    <ListItem>
-                        <ListItemIcon>
-                            <CoffeeIcon />
-                        </ListItemIcon>
-                        <ListItemText
-                            primary="Petit-déjeuner"
-                        />
-                    </ListItem>
-                    <ListItem>
-                        <TextField id="add-breakfast" value={breakfast} label="Petit-déjeuner" variant="filled" onChange={handleBFChange} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon>
-                            <FastfoodIcon />
-                        </ListItemIcon>
-                        <ListItemText
-                            primary="Déjeuner"
-                        />
-                    </ListItem>
-                    <ListItem>
-                        <TextField id="add-breakfast" value={lunch} label="Déjeuner" variant="filled" onChange={handleLChange} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon>
-                            <DinnerDiningIcon />
-                        </ListItemIcon>
-                        <ListItemText
-                            primary="Dîner"
-                        />
-                    </ListItem>
-                    <ListItem>
-                        <TextField id="add-dinner" value={dinner} label="Dîner" variant="filled" onChange={handleDChange} />
-                    </ListItem>
-                </List>
-            </CardContent>
-        </Card>
+                    <Divider />
+                    <List>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CoffeeIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="Petit-déjeuner"
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <TextField focused={(d.breakfast.name != undefined) ? true : false} id="add-breakfast" value={breakfast} label="Petit-déjeuner" variant="filled" onChange={handleBFChange(d)} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <FastfoodIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="Déjeuner"
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <TextField focused={(d.lunch.name != undefined) ? true : false} id="add-lunch" value={lunch} label="Déjeuner" variant="filled" onChange={handleLChange(d)} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <DinnerDiningIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="Dîner"
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <TextField focused={(d.dinner.name != undefined) ? true : false} id="add-dinner" value={dinner} label="Dîner" variant="filled" onChange={handleDChange(d)} />
+                        </ListItem>
+                    </List>
+                </CardContent>
+            </Card>
+            <Button color="success" variant="contained" onClick={handleFinish(d)} fullWidth>Terminer</Button>
+        </div>
     );
 }
--- frontend/src/state/actions/day.actions.ts
+++ frontend/src/state/actions/day.actions.ts
@@ -11,3 +11,33 @@ export const getDay = (slug: string) => {
             .catch((err) => console.log(err))
     }
 }
+
+export const editBreakfast = (slug: string, breakfast_slug :string) => {
+    return (dispatch) => {
+        return axios.get("http://192.168.0.10:5000/wye/days/"+slug+"?breakfast="+breakfast_slug)
+            .then((res) => {
+                dispatch({ type: GET_DAY, payload: res.data })
+            })
+            .catch((err) => console.log(err))
+    }
+}
+
+export const editLunch = (slug: string, lunch_slug :string) => {
+    return (dispatch) => {
+        return axios.get("http://192.168.0.10:5000/wye/days/"+slug+"?lunch="+lunch_slug)
+            .then((res) => {
+                dispatch({ type: GET_DAY, payload: res.data })
+            })
+            .catch((err) => console.log(err))
+    }
+}
+
+export const editDinner = (slug: string, dinner_slug :string) => {
+    return (dispatch) => {
+        return axios.get("http://192.168.0.10:5000/wye/days/"+slug+"?dinner="+dinner_slug)
+            .then((res) => {
+                dispatch({ type: GET_DAY, payload: res.data })
+            })
+            .catch((err) => console.log(err))
+    }
+}
